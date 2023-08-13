d51c1adacd5976e459997d90ea00a556fd22ad2b
Hussain Nawaz Lalee <hussainnawazlalee@gmail.com>
2023-08-13 17:17:18 +0500

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve("src", "components/index.jsx"),
+      name: "kappa",
+      fileName: (format) => `kappa.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
